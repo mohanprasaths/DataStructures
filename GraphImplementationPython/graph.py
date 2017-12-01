b3101ddb622220bc9517699a5575679675d377a9
mohanprasaths <mohanprasath@live.com>
2017-12-01 22:16:21 +0530

    Adjacency set graph added

--- GraphImplementationPython/graph.py
+++ GraphImplementationPython/graph.py
@@ -79,6 +79,82 @@ class AdjacencyMatrixGraph(Graph):
 			for v in self.get_adjacent_vertices(i):
 				print(i, "-->", v)
 
+#######################################################################
+#
+# A single node in a graph represented by an adjacency set. Every node
+# has a vertex id
+# Each node is associated with a set of adjacent vertices
+#
+#######################################################################
+class Node:
+	def __init__(self, vertexId):
+		self.vertexId = vertexId
+		self.adjacency_set = set()
+
+	def add_edge(self, v):
+		if self.vertexId == v:
+			raise ValueError("The vertex %d cannot be adjacent to itself" % v)
+
+		self.adjacency_set.add(v)
+
+	def get_adjacent_vertices(self):
+		return sorted(self.adjacency_set)
+
+
+
+#######################################################################
+#
+# Represents a graph as an adjacency set. A graph is a list of Nodes
+# and each Node has a set of adjacent vertices.
+# This graph in this current form cannot be used to represent
+# weighted edges only unweighted edges can be represented
+#
+#######################################################################
+class AdjacencySetGraph(Graph):
+
+	def __init__(self, numVertices, directed=False):
+		super(AdjacencySetGraph, self).__init__(numVertices, directed)
+
+		self.vertex_list = []
+		for i in range(numVertices):
+			self.vertex_list.append(Node(i))
+
+	def add_edge(self, v1, v2, weight=1):
+		if v1 >= self.numVertices or v2 >= self.numVertices or v1 < 0 or v2 < 0:
+			raise ValueError("Vertices %d and %d are out of bounds" % (v1, v2))
+
+		if weight != 1:
+			raise ValueError("An adjacency set cannot represent edge weights >1")
+
+		self.vertex_list[v1].add_edge(v2)
+		if self.directed == False:
+			self.vertex_list[v2].add_edge(v1)
+
+
+	def get_adjacent_vertices(self, v):
+		if v < 0 or v >= self.numVertices:
+			raise ValueError("Cannot access vertex %d" % v)
+
+		return self.vertex_list[v].get_adjacent_vertices()
+
+	def get_indegree(self, v):
+		if v < 0 or v >= self.numVertices:
+			raise ValueError("Cannot access vertex %d" % v)
+
+		indegree = 0
+		for i in range(self.numVertices):
+			if v in self.get_adjacent_vertices(i):
+				indegree = indegree + 1
+
+		return indegree
+
+	def get_edge_weight(self, v1, v2):
+		return 1
+
+	def display(self):
+		for i in range(self.numVertices):
+			for v in self.get_adjacent_vertices(i):
+				print(i, "-->", v)
 
 g = AdjacencySetGraph(4)
 g.add_edge(0, 1)
